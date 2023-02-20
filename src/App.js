97e98950dabb8af8dad570eea3aa2b50e8ba73d6
Burak Kose <burakkose011@icloud.com>
2023-02-20 18:41:26 +0300

    tamamlandi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./app.css";
 
 function App() {
+
+  const [minVal, setMinVal] = useState(10);
+  const [maxVal, setMaxVal] = useState(20);
+  const [randomNum, setRandomNum] = useState(5);
+
+  const rastgeleSayi=()=>{
+    setRandomNum(Math.floor(Math.random()*(maxVal - minVal +1) + minVal))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero">
+      <div className="container">
+        <div className="randomNum">
+          <p>
+            Random Number : <span>{randomNum}</span>
+          </p>
+        </div>
+        <div className="numContainer">
+          <div>
+            <p>Min : </p>
+            <input type="number" value={minVal} 
+            onChange={e => setMinVal(+e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <p>Max : </p>
+            <input type="number" value={maxVal}
+            onChange={e => setMaxVal(+e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <button onClick={rastgeleSayi} >Get Random Number</button>
+      </div>
     </div>
   );
 }
